d6486bb2862f20948a575404a67f96d1211fbaf1
Nastya <staprost.22@gmail.com>
2023-05-30 00:06:40 +0200

    Добавьте файлы проекта.

---
+++ FuzzyLog3/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace FuzzyLog3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(337, 295);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(17, 321);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.integral_textbox_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(773, 246);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(14, 395);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Замовлення порту / Залишок палива";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(561, 295);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 67);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Нове замовлення";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(14, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кількість палива в танкері:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = global::FuzzyLog3.Properties.Resources.cruise;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(805, 503);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FuzzyLog3/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FuzzyLog3
+{
+    public partial class Form1 : Form
+    {
+        private void integral_textbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8 && e.KeyChar != 45 && e.KeyChar != 44)
+                e.Handled = true;
+        }
+
+        float a1, a2, a3;
+        float b1, b2, b3;
+        float Coord = 0;
+        float v = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void button1_Click(object sender, EventArgs e) //Обрахувати (кнопка)
+        {
+            order();
+            draw();
+            button1.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e) { } //"Замовлення порту / Залишок палива"
+
+        private void label2_Click(object sender, EventArgs e) { } //"Кількість палива в танкері"
+
+        private void pictureBox1_Click(object sender, EventArgs e) { } //Схема
+
+        private void button2_Click(object sender, EventArgs e) //Нове замовлення (кнопка)
+        {
+            Random rand = new Random();
+            if (float.Parse(textBox1.Text) >= 50)
+            {
+                b1 = rand.Next(1, ((int)v / 4));
+                b2 = rand.Next((int)b1, ((int)v / 4));
+                b3 = rand.Next((int)b2, ((int)v / 4));
+                draw();
+                button1.Enabled = true;
+                textBox1.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Значення повинно бути більше 50", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Text = null;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //Кількість палива в танкері
+        {
+            float a;
+            if (float.TryParse(textBox1.Text, out a))
+            {
+                    a3 = a2 = a1 = float.Parse(textBox1.Text);
+                    Coord = 574 / (a1 * 1.1f);
+                    v = a1;
+            }
+            else
+            {               
+                MessageBox.Show("Введіть коректне значення!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+                textBox1.Text = null;
+            }
+        }
+
+        private void order()
+        {  
+         if (b3 <= a1)
+            {
+                a3 -= b1;
+                a2 -= b2;
+                a1 -= b3;
+            }
+            else {
+                MessageBox.Show("Not enough fuel", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Enabled = true;
+            }
+           
+        }
+        private void draw()
+        { 
+            label1.Text = "Залишок палива на танкері - "+"["+a1+", "+a2+", "+a3+"]"+"\nЗамовлення порту - "+
+                "[" + b1 + ", " + b2 + ", " + b3 + "]";
+            Pen AxisPen = new Pen(Color.Black, 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.FromArgb(185,209,233));
+            g.DrawLine(AxisPen, 4, 0, 4, 200);
+            g.DrawLine(AxisPen, 0, 180, 580, 180);
+
+            Pen FuelPen = new Pen(Color.Blue, 3);
+            g.DrawLine(FuelPen, a1 * Coord, 180, a2 * Coord, 100);
+            g.DrawLine(FuelPen, a2 * Coord, 100, a3 * Coord, 180);
+
+            Pen OrderPen = new Pen(Color.Red, 3);
+            g.DrawLine(OrderPen, b1 * Coord, 180, b2 * Coord, 100);
+            g.DrawLine(OrderPen, b2 * Coord, 100, b3 * Coord, 180);
+
+        }
+    }
+}
